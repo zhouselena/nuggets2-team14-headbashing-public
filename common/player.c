80872a625b2be9e3742ab15310e0d32a2e3b8897
Selena Y. Zhou <f006h51@plank.thayer.dartmouth.edu>
2023-05-30 17:06:23 -0400

    finished player comments'

--- common/player.c
+++ common/player.c
@@ -15,7 +15,7 @@
 
 /**************** file-local global variables ****************/
 
-static char nextPlayer = 'A';
+static char nextPlayer = 'A';            // to create unique ID
 
 /**************** global types ****************/
 
@@ -34,6 +34,8 @@ typedef struct player {
 
 /* create and delete */
 
+/**************** player_new ****************/
+/* see player.h for description */
 player_t* player_new() {
     
     player_t* player = malloc(sizeof(player_t));
@@ -52,21 +54,29 @@ player_t* player_new() {
 
 }
 
+/**************** player_delete ****************/
+/* see player.h for description */
 void player_delete(player_t* player) {
     free(player->playerName);
     free(player);
 }
 
-/* set functions */
+/* setters */
 
+/**************** player_setAddress ****************/
+/* see player.h for description */
 void player_setAddress(player_t* player, addr_t address) {
     player->playerAddress = address;
 }
 
+/**************** player_setName ****************/
+/* see player.h for description */
 void player_setName(player_t* player, char* name) {
     player->playerName = name;
 }
 
+/**************** player_initializeGridAndLocation ****************/
+/* see player.h for description */
 void player_initializeGridAndLocation(player_t* player, grid_t* visibleGrid, grid_t* goldMap, int locationX, int locationY) {
     player->visibleMap = visibleGrid;
     player->playerXLocation = locationX;
@@ -79,21 +89,28 @@ void player_initializeGridAndLocation(player_t* player, grid_t* visibleGrid, gri
 
 /* update functions */
 
+/**************** player_moveUpAndDown ****************/
+/* see player.h for description */
 void player_moveUpAndDown(player_t* player, int steps, char resetMapSpot) {
     grid_set(player->visibleMap, player->playerYLocation, player->playerXLocation, resetMapSpot);
     player->playerYLocation += steps;
     grid_set(player->visibleMap, player->playerYLocation, player->playerXLocation, GRID_PLAYER_ME);
 }
+/**************** player_moveLeftAndRight ****************/
+/* see player.h for description */
 void player_moveLeftAndRight(player_t* player, int steps, char resetMapSpot) {
     grid_set(player->visibleMap, player->playerYLocation, player->playerXLocation, resetMapSpot);
     player->playerXLocation += steps;
     grid_set(player->visibleMap, player->playerYLocation, player->playerXLocation, GRID_PLAYER_ME);
 }
+/**************** player_foundGoldNuggets ****************/
+/* see player.h for description */
 void player_foundGoldNuggets(player_t* player, int foundGold) {
     player->numGold += foundGold;
 }
 
-// called after player x and y are updated
+/**************** player_updateVisibility ****************/
+/* see player.h for description */
 void player_updateVisibility(player_t* player, grid_t* fullMap, grid_t* goldMap) {
     // grid_overlay(const grid_t* base, const grid_t* overlay, const grid_t* mask, grid_t* out)
     // base: player->visibleMap
@@ -113,6 +130,8 @@ void player_updateVisibility(player_t* player, grid_t* fullMap, grid_t* goldMap)
     grid_delete(updatedVisible);
 }
 
+/**************** player_serverMapUpdate ****************/
+/* see player.h for description */
 void player_serverMapUpdate(player_t* player, game_t* game) {
     // grid_overlay(const grid_t* base, const grid_t* overlay, const grid_t* mask, grid_t* out)
     // base: player->visibleMap
@@ -130,34 +149,50 @@ void player_serverMapUpdate(player_t* player, game_t* game) {
 
 /* getter functions */
 
+/**************** player_getAddr ****************/
+/* see player.h for description */
 addr_t player_getAddr(player_t* player) {
     return player->playerAddress;
 }
 
+/**************** player_getID ****************/
+/* see player.h for description */
 char player_getID(player_t* player) {
     return player->playerID;
 }
 
+/**************** player_getName ****************/
+/* see player.h for description */
 char* player_getName(player_t* player) {
     return player->playerName;
 }
 
+/**************** player_getXLocation ****************/
+/* see player.h for description */
 int player_getXLocation(player_t* player) {
     return player->playerXLocation;
 }
 
+/**************** player_getYLocation ****************/
+/* see player.h for description */
 int player_getYLocation(player_t* player) {
     return player->playerYLocation;
 }
 
+/**************** player_getMap ****************/
+/* see player.h for description */
 grid_t* player_getMap(player_t* player) {
     return player->visibleMap;
 }
 
+/**************** player_getVisibleGold ****************/
+/* see player.h for description */
 grid_t* player_getVisibleGold(player_t* player) {
     return player->visibleGold;
 }
 
+/**************** player_getGold ****************/
+/* see player.h for description */
 int player_getGold(player_t* player) {
     return player->numGold;
 }
